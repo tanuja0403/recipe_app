1a8881e836fefadd4d59d788351678269391387e
AbhijitKr <abhijitkr600@gmail.com>
2024-01-30 22:21:26 +0530

    Holds recipeList and maintains the loading state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { GlobalContext } from "../context";
 
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -4,25 +4,32 @@ export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
   const [searchParam, setSearchParam] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
 
   const api = import.meta.env.VITE_FORKIFY_API;
 
   async function handleSearch(e) {
     e.preventDefault();
+    setLoading(true);
     try {
       const response = await fetch(
         `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}&key=${api}`
       );
       const data = await response.json();
-      console.log(data);
+      if (data?.data?.recipes) {
+        setRecipeList(data.data.recipes);
+      }
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   }
 
   return (
     <GlobalContext.Provider
-      value={{ searchParam, setSearchParam, handleSearch }}
+      value={{ searchParam, setSearchParam, handleSearch, loading, recipeList }}
     >
       {children}
     </GlobalContext.Provider>
